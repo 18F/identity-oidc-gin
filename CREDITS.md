7fa9f3beecca2149f0709ed1cfadaa5d1f23f223
MJ Rossetti <s2t2@users.noreply.github.com>
2018-02-19 18:31:51 -0500

    Views (#1)
    * Load static assets
    
    * Add profile page
    
    * Pass user information to profile
    
    * Complete mock navigation

---
+++ CREDITS.md
@@ -0,0 +1,25 @@
+# Credits, Notes, and Reference
+
+## Go Language
+
+  + [How to Write Go Code](https://golang.org/doc/code.html)
+
+### Datatypes
+
+  + [Go by Example: Arrays](https://gobyexample.com/arrays)
+  + [Unexported Struct Identifiers](https://www.ardanlabs.com/blog/2014/03/exportedunexported-identifiers-in-go.html)
+
+### Templates
+
+  + [Intro to Templates](https://www.calhoun.io/intro-to-templates-p3-functions/)
+  + [Build a Web Application: Templates](https://astaxie.gitbooks.io/build-web-application-with-golang/en/07.4.html)
+  + [Golang Templates Cheatsheet](https://curtisvermeeren.github.io/2017/09/14/Golang-Templates-Cheatsheet.html)
+
+## Gin Web Framework
+
+  + [Gin Source Code](https://github.com/gin-gonic/gin)
+  + [Iteration in Templates](https://github.com/gin-gonic/gin/issues/600#issuecomment-214023292)
+
+## Assets
+
+Stylesheets and images borrowed from the [Sinatra SAML Client](https://github.com/18F/identity-sp-sinatra/).
--- app.go
+++ app.go
@@ -1,26 +1,58 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
+  "github.com/gin-gonic/gin"
+  "net/http"
 )
+
+type User struct {
+  Email string
+  FamilyName string
+  GivenName string
+}
+
+func apiPing(context *gin.Context) {
+  context.JSON(200, gin.H{"message": "pong",})
+}
+
+func renderIndex(context *gin.Context) {
+  context.HTML(http.StatusOK, "index.tmpl", gin.H{"title": "Login.gov OIDC Client (Gin)",})
+}
+
+func renderProfile(context *gin.Context) {
+  var blocks [5]int
+  user := User{Email: "test.user@gmail.com", GivenName: "Test", FamilyName:"User"}
+
+  context.HTML(http.StatusOK, "profile.tmpl", gin.H{
+    "title": "Profile Page",
+    "blocks": blocks,
+    "user": user,
+  })
+}
+
+func tempRedirectToProfile(context *gin.Context)  {
+  context.Redirect(http.StatusTemporaryRedirect, "/profile")
+}
+
+func tempRedirectHome(context *gin.Context){
+  context.Redirect(http.StatusTemporaryRedirect, "/")
+}
 
 func main() {
 	router := gin.Default()
 
-	router.LoadHTMLGlob("views/*")
+	router.LoadHTMLGlob("views/*") // load views
+	router.Static("/assets", "./assets") // load static assets
 
-	router.GET("/", func(context *gin.Context) {
-		context.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Hello World",
-		})
-	})
+	router.GET("/", renderIndex)
+	router.GET("/profile", renderProfile)
 
-	router.GET("/ping", func(context *gin.Context) {
-		context.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	router.GET("/auth/login-gov/login/loa-1", tempRedirectToProfile)
+	router.GET("/auth/login-gov/login/loa-3", tempRedirectToProfile)
+	router.GET("/auth/login-gov/logout", tempRedirectHome)
+	router.GET("/auth/login-gov/logout/rp", tempRedirectHome)
+
+	router.GET("/ping", apiPing)
 
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
